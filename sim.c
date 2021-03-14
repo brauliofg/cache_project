9b8019d23998f8c6ef1069b665c9d9b09b9feda6
Braulio Garcia <garciafroy@me.com>
2021-03-14 18:05:26 -0500

    added printing of parsed command line

--- sim.c
+++ sim.c
@@ -35,6 +35,7 @@ int main(int argc, char *argv[]){
             return -1;
         }
     }
+    printInput(input);
 
     //Free struct that holds information from command line
     freeInput(input);
--- sim.h
+++ sim.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct InputData
 {
@@ -24,4 +25,22 @@ void freeInput(InputData *input){
     free(input->fileName);
     free(input->replacementPolicy);
     free(input);
+}
+
+void printInput(InputData *input){
+    printf("Cache Simulator CS 3853 Spring 2021 - Group #8\n\n");
+    printf("Trace File: %s\n", input->fileName);
+    printf("\n***** Cache Input Parameters *****\n\n");
+    printf("Cache Size:             %d KB\n", input->cacheSize);
+    printf("Block Size:             %d bytes\n", input->blockSize);
+    printf("Associativity:          %d\n", input->associativity);
+    if(strcmp(input->replacementPolicy, "RR")==0){
+        printf("Replacement Policy:     Round-robin\n");
+    }
+    else if(strcmp(input->replacementPolicy, "RND")==0){
+        printf("Replacement Policy:     Random\n");
+    }
+    else if(strcmp(input->replacementPolicy, "LRU")==0){
+        printf("Replacement Policy:     Least Recently Used\n");
+    }
 }
